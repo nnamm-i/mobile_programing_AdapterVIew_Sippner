8f85edd3cd1a14dcd510d189e0ad619599d5fe08
안정남 <ruru0131@naver.com>
2026-04-27 23:03:50 +0900

    제출물2

--- app/src/main/java/com/example/adapterview_spinner/MainActivity.kt
+++ app/src/main/java/com/example/adapterview_spinner/MainActivity.kt
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
